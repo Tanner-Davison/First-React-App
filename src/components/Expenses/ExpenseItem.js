d9d320ea900e7bd02f292a432c0ee2722b7e4a1f
tannerdavison <tboydavison@gmail.com>
2023-07-01 19:57:57 -0600

    first-react-app save

---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,37 @@
+import React,{useState} from 'react'
+import ExpenseDate from "./ExpenseDate.js";
+import Card from '../UI/Card.js';
+import "./ExpenseItem.css";
+
+
+const ExpenseItem = (props) => {
+	const [title, setTitle] = useState(props.title);
+	const [counter, setCounter] = useState(1);
+
+	const clickHandler = () => {
+		
+		if (counter === 1) {
+			setTitle("updated");
+			setCounter(0);
+		} else if (counter < 1) {
+			setTitle(props.title);
+			setCounter(1);
+			
+		}
+		console.log(counter);
+	};
+	return (
+		<Card className='expense-item'>
+			<ExpenseDate date={props.date} />
+			<div className='expense-item__description'>
+				<h2>{title}</h2>
+				<div className='expense-item__price'>
+					<h2>{props.amount}</h2>
+				</div>
+				<button onClick={clickHandler}>save title</button>
+			</div>
+		</Card>
+	);
+};
+
+export default ExpenseItem;
